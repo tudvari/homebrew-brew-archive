2997c989cb22b5256b0bd36da3db8389418e1b7f
tudvari <tudvari@tudvari.com>
2020-12-31 16:30:46 +0100

    spacing fix

--- Casks/dotnet-sdk@3.1.4.rb
+++ Casks/dotnet-sdk@3.1.4.rb
@@ -11,12 +11,14 @@ cask "dotnet-sdk@3.1.4" do
   appcast "https://www.microsoft.com/net/download/macos"
   name ".NET Core SDK"
   homepage "https://www.microsoft.com/net/core#macos"
+
   #
   # conflicts_with cask: [
   #  "dotnet",
   #  "dotnet-preview",
   #  "dotnet-sdk-preview",
   # ]
+  
   depends_on macos: ">= :sierra"
 
   pkg "dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
