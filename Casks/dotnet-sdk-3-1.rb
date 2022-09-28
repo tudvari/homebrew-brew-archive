06015e0cc88bbab7949e31cb4308cd6a3df8bd49
tudvari <tudvari@tudvari.com>
2022-09-28 13:55:31 +0200

    update dotnet-sdk to version 3.1.416

--- Casks/dotnet-sdk-3-1.rb
+++ Casks/dotnet-sdk-3-1.rb
@@ -4,8 +4,8 @@ cask "dotnet-sdk-3-1" do
     url "https://download.visualstudio.microsoft.com/download/pr/7430e32b-092b-4448-add7-2dcf40a7016d/1076952734fbf775062b48344d1a1587/dotnet-sdk-2.2.402-osx-x64.pkg"
     # sha256 "e74d816bc034d0fcdfa847286a6cad097227d4864da1c97fe801012af0c26341"
   else
-    version "3.1.403,b4db3ee6-91da-4dd3-a279-37c45d9627b7:fad88a7335d374d6e4d53aef085f4b24"
-    url "https://download.visualstudio.microsoft.com/download/pr/b4db3ee6-91da-4dd3-a279-37c45d9627b7/fad88a7335d374d6e4d53aef085f4b24/dotnet-sdk-3.1.403-osx-x64.pkg"
+    version "3.1.416,895b7e35-b1c2-4d1d-8d7f-f82f4ae70eb7:9470e66ca28443328cbaa36963ff83f8"
+    url "https://download.visualstudio.microsoft.com/download/pr/895b7e35-b1c2-4d1d-8d7f-f82f4ae70eb7/9470e66ca28443328cbaa36963ff83f8/dotnet-sdk-3.1.416-osx-x64.tar.gz"
     # sha256 "6b4f384b8a724bab114e0685285a98003ed21227acfb3c6ff0dc083d981812fc"
   end
 
