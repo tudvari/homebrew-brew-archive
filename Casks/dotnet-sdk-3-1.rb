d3c17472f1d7d7e8036941933f039c489a7d8786
tudvari <tudvari@tudvari.com>
2022-09-28 15:35:09 +0200

    dotnet-sdk cask fix

--- Casks/dotnet-sdk-3-1.rb
+++ Casks/dotnet-sdk-3-1.rb
@@ -21,9 +21,9 @@ cask "dotnet-sdk-3-1" do
   binary "/usr/local/share/dotnet/dotnet"
 
   uninstall pkgutil: [
-                        "com.microsoft.dotnet.*",
-                        "com.microsoft.netstandard.pack.targeting.*",
-                     ],
+              "com.microsoft.dotnet.*",
+              "com.microsoft.netstandard.pack.targeting.*",
+            ],
             delete:  [
               "/etc/paths.d/dotnet",
               "/etc/paths.d/dotnet-cli-tools",
