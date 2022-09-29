f3aa53b42ce66d6afc8e372246489830bdb7265a
tudvari <tudvari@tudvari.com>
2022-09-29 10:44:47 +0200

    remove .net2.x

--- Casks/dotnet-sdk-3-1.rb
+++ Casks/dotnet-sdk-3-1.rb
@@ -1,13 +1,7 @@
 cask "dotnet-sdk-3-1" do
-  if MacOS.version <= :sierra
-    version "2.2.402,7430e32b-092b-4448-add7-2dcf40a7016d:1076952734fbf775062b48344d1a1587"
-    url "https://download.visualstudio.microsoft.com/download/pr/7430e32b-092b-4448-add7-2dcf40a7016d/1076952734fbf775062b48344d1a1587/dotnet-sdk-2.2.402-osx-x64.pkg"
-    # sha256 "e74d816bc034d0fcdfa847286a6cad097227d4864da1c97fe801012af0c26341"
-  else
-    version "3.1.416,895b7e35-b1c2-4d1d-8d7f-f82f4ae70eb7:9470e66ca28443328cbaa36963ff83f8"
-    url "https://download.visualstudio.microsoft.com/download/pr/895b7e35-b1c2-4d1d-8d7f-f82f4ae70eb7/9470e66ca28443328cbaa36963ff83f8/dotnet-sdk-3.1.416-osx-x64.tar.gz"
+    version "3.1.416"
+    url "https://download.visualstudio.microsoft.com/download/pr/895b7e35-b1c2-4d1d-8d7f-f82f4ae70eb7/9470e66ca28443328cbaa36963ff83f8/dotnet-sdk-{version}-osx-x64.tar.gz"
     # sha256 "6b4f384b8a724bab114e0685285a98003ed21227acfb3c6ff0dc083d981812fc"
-  end
 
   sha256 :no_check
 
@@ -17,7 +11,7 @@ cask "dotnet-sdk-3-1" do
 
   depends_on macos: ">= :sierra"
 
-  pkg "dotnet-sdk-3.1.416-osx-x64.tar.gz"
+  pkg "dotnet-sdk-{version}-osx-x64.tar.gz"
   binary "/usr/local/share/dotnet/dotnet"
 
   uninstall pkgutil: [
