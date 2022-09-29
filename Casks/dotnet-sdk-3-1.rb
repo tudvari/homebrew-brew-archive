20d94022df86282bbf9e50f0554b56c1eb2e5619
tudvari <tudvari@tudvari.com>
2022-09-29 10:53:26 +0200

    style fix

--- Casks/dotnet-sdk-3-1.rb
+++ Casks/dotnet-sdk-3-1.rb
@@ -1,10 +1,10 @@
 cask "dotnet-sdk-3-1" do
   version "3.1.416"
-  url "https://download.visualstudio.microsoft.com/download/pr/895b7e35-b1c2-4d1d-8d7f-f82f4ae70eb7/9470e66ca28443328cbaa36963ff83f8/dotnet-sdk-{version}-osx-x64.tar.gz"
   # sha256 "6b4f384b8a724bab114e0685285a98003ed21227acfb3c6ff0dc083d981812fc"
 
   sha256 :no_check
 
+  url "https://download.visualstudio.microsoft.com/download/pr/895b7e35-b1c2-4d1d-8d7f-f82f4ae70eb7/9470e66ca28443328cbaa36963ff83f8/dotnet-sdk-{version}-osx-x64.tar.gz"
   appcast "https://www.microsoft.com/net/download/macos"
   name ".NET Core SDK"
   homepage "https://www.microsoft.com/net/core#macos"
